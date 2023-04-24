4d986edb2b68a14ac2e5eb6311b0a23d1038c74c
Alexa Williams <alexawill23@gmail.com>
2023-04-24 15:16:18 -0400

    input firstname

---
+++ LectureDemos/FirstCode.js
@@ -0,0 +1,21 @@
+//The following is a script 
+//That does a few things
+
+/*
+The following is a script 
+that does a few things 
+
+*/
+
+var firstName;
+
+firstName = "Alexa" ;
+
+console.log ("Your name is" + firstName);
+
+
+// var greeting; 
+
+// greeting = "Your name is" + firstName; // This line will assign the result of an expression to the greeting variable 
+
+// console.log(greeting);
---
+++ LectureDemos/fourthcode.js
@@ -0,0 +1,13 @@
+"use strict"; 
+var firstName
+firstName = "Alexa"; 
+var birthcity;
+birthcity = "Bronx"; 
+var birthstate;
+birthstate= "New York"; 
+
+console.log("Your name is "+ firstName); 
+
+console.log ("Your city of birth is " + birthcity);
+
+console.log ("Your birth state is " + birthstate);
